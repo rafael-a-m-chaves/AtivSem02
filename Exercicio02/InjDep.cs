7521d2cfd466338ed9dac819c78c7ac7c1460356
rafael chaves <rafaelaugustochaves@hotmail.com>
2022-02-28 12:22:28 -0300

    injeção de dependencia conluido

---
+++ Exercicio02/InjDep.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio01
+{
+    public class InjDep
+    {
+    }
+
+    public interface IBancos
+    {
+        public void RecebendoDouble(double parametro);
+
+        public double RetornandoDouble();
+    }
+
+    public class Santander : IBancos
+    {
+        private double SaldoInicial = 1251146.24;
+        public double SaldoAtual { get; set; }
+
+        public Santander()
+        {
+            SaldoAtual = SaldoInicial;
+        }
+
+        public void RecebendoDouble(double parametro)
+        {
+            SaldoAtual += parametro;
+            Console.WriteLine("Novo saldo no banco Santander apos transferencia é R$" + SaldoAtual);
+        }
+
+        public double RetornandoDouble()
+        {
+            var retirada = 1000.00;
+            Console.WriteLine("Iniciando o processo de tranferencia");
+            Console.WriteLine("Será retirado do Banco Santander o valor de R${0} do saldo atual de R${1} para ser transferido", retirada, SaldoAtual);
+            SaldoAtual -= retirada;
+            return retirada;
+        }
+    }
+
+    public class Itau : IBancos
+    {
+        private double SaldoInicial = 125444.24;
+        public double SaldoAtual { get; set; }
+
+        public Itau()
+        {
+            SaldoAtual = SaldoInicial;
+        }
+
+        public void RecebendoDouble(double parametro)
+        {
+            SaldoAtual += parametro;
+            Console.WriteLine("Novo saldo no banco Itau apos transferencia é R$" + SaldoAtual);
+        }
+
+        public double RetornandoDouble()
+        {
+            var retirada = 2000.00;
+            Console.WriteLine("Iniciando o processo de tranferencia");
+            Console.WriteLine("Será retirado do Banco Itau o valor de R${0} do saldo atual de R${1} para ser transferido", retirada, SaldoAtual);
+            SaldoAtual -= retirada;
+            return retirada;
+        }
+    }
+
+    public class Caixa : IBancos
+    {
+        private double SaldoInicial = 1254226.24;
+        public double SaldoAtual { get; set; }
+
+        public Caixa()
+        {
+            SaldoAtual = SaldoInicial;
+        }
+
+        public void RecebendoDouble(double parametro)
+        {
+            SaldoAtual += parametro;
+            Console.WriteLine("Novo saldo no banco Caixa é apos transferencia R$" + SaldoAtual);
+        }
+
+        public double RetornandoDouble()
+        {
+            var retirada = 3000.00;
+            Console.WriteLine("Iniciando o processo de tranferencia");
+            Console.WriteLine("Será retirado do Banco Caixa o valor de R${0} do saldo atual de R${1} para ser transferido", retirada, SaldoAtual);
+            SaldoAtual -= retirada;
+            return retirada;
+        }
+    }
+
+    public class TranferenciaBancaria
+    {
+        private IBancos BancoRetira;
+        private IBancos BancoRecebe;
+
+        public TranferenciaBancaria()
+        {
+
+        }
+
+        public TranferenciaBancaria(IBancos bancoRetira, IBancos bancoRecebe)
+        {
+            this.BancoRetira = bancoRetira;
+            this.BancoRecebe = bancoRecebe;
+        }
+        
+        public void IniciarTranferencia()
+        {
+            double valor = BancoRetira.RetornandoDouble();
+            BancoRecebe.RecebendoDouble(valor);
+        }
+
+    }
+}
--- Exercicio02/Program.cs
+++ Exercicio02/Program.cs
@@ -15,12 +15,56 @@ namespace Exercicio01
             if (aluno != null)
             {
                 Console.WriteLine("A pesquisa retornou o seguinte aluno");
-                Console.WriteLine(aluno);
+                Console.WriteLine("nome: {0}   idade: {1}    Codigo aluno: {2}", aluno.Nome, aluno.Idade, aluno.Codigo);
             }
             else
             {
                 Console.WriteLine("A pesquisa não retornou resultados");
             }
+            
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Santander santander = new Santander();
+            Itau itau = new Itau();
+            Caixa caixa = new Caixa();
+
+            TranferenciaBancaria tranferencia1 = new TranferenciaBancaria(santander, itau);
+            tranferencia1.IniciarTranferencia();
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            TranferenciaBancaria tranferencia2 = new TranferenciaBancaria(santander, caixa);
+            tranferencia2.IniciarTranferencia();
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            TranferenciaBancaria tranferencia3 = new TranferenciaBancaria(itau, caixa);
+            tranferencia3.IniciarTranferencia();
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            TranferenciaBancaria tranferencia4 = new TranferenciaBancaria(itau, santander);
+            tranferencia4.IniciarTranferencia();
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            TranferenciaBancaria tranferencia5 = new TranferenciaBancaria(caixa, santander);
+            tranferencia5.IniciarTranferencia();
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            TranferenciaBancaria tranferencia6 = new TranferenciaBancaria(caixa, itau);
+            tranferencia6.IniciarTranferencia();
+
+            Console.WriteLine();
+            Console.WriteLine();
+
             Console.ReadKey();
         }
     }
